acc952ca98b6c1433ae7d9eab8f34c3b7c84bdbc
Med Ayoub Achour <you@example.com>
2023-07-18 11:41:15 +0100

    Updated the search header component

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,11 +1,11 @@
 "use client";
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { BsFillMicFill } from 'react-icons/bs';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
 
 export default function HomeSearch() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const router = useRouter();
   const [randomSearchLoading, setRandomSearchLoading] = useState(false);
 
@@ -18,12 +18,14 @@ export default function HomeSearch() {
   async function randomSearch() {
     setRandomSearchLoading(true);
     try {
-      const response = await fetch('https://random-word-api.herokuapp.com/word').then((res) => res.json());
+      const response = await fetch(
+        "https://random-word-api.herokuapp.com/word"
+      ).then((res) => res.json());
       const randomWord = response[0];
       if (!randomWord) return;
       router.push(`/search/web?searchTerm=${randomWord}`);
     } catch (error) {
-      console.error('Error occurred during random search:', error);
+      console.error("Error occurred during random search:", error);
     } finally {
       setRandomSearchLoading(false);
     }
@@ -31,9 +33,20 @@ export default function HomeSearch() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
-        <AiOutlineSearch className="text-xl text-gray-500 mr-3" onClick={handleSubmit} />
-        <input type="text" className="flex-grow focus:outline-none" onChange={(e) => setInput(e.target.value)} value={input} />
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      >
+        <AiOutlineSearch
+          className="text-xl text-gray-500 mr-3"
+          onClick={handleSubmit}
+        />
+        <input
+          type="text"
+          className="flex-grow focus:outline-none"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
         <BsFillMicFill className="text-xl text-gray-500 mr-3" />
       </form>
 
@@ -41,11 +54,20 @@ export default function HomeSearch() {
         <button className="btn" type="submit" onClick={handleSubmit}>
           Google Search
         </button>
-        <button disabled={randomSearchLoading} className="btn flex items-center justify-center disabled:opacity-80" type="submit" onClick={randomSearch}>
+        <button
+          disabled={randomSearchLoading}
+          className="btn flex items-center justify-center disabled:opacity-80"
+          type="submit"
+          onClick={randomSearch}
+        >
           {randomSearchLoading ? (
-            <img src="/spinner.svg" alt="Loading..." className="h-6 text-center" />
+            <img
+              src="/spinner.svg"
+              alt="Loading..."
+              className="h-6 text-center"
+            />
           ) : (
-            'I\'m Feeling Lucky'
+            "I'm Feeling Lucky"
           )}
         </button>
       </div>
